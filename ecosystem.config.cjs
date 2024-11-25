db9359a55a62118ebffede9e8fb348ba034baf27
shishikami <2428076728@qq.com>
2024-11-26 00:49:14 +0800

    chore: 部署相关文件命令行修改

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,6 +1,12 @@
 module.exports = {
   apps : [{
-    name   : "chat-service",
-    script : "./dist/index.js"
+    name: "chat-service",
+    script: "./dist/index.js",
+    env_production: {
+      NODE_ENV: 'production',
+      PORT: 3000,
+      // COZE_API_TOKEN: 'pat_LTc2sxmJWWYiSpeZVsPCuIXocuccruRxJpGYwJ40wad3iEcNW59vPCrpgB9qMCUp',
+      COZE_API_TOKEN: 'pat_02mAD25CDzcjx0hb1hW8T1pgfVIRkrX0DCAfuoOd7f5asEQgwDtaCsjonlWmaHWY'
+    }
   }]
 }
